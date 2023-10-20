9f5d95b16ce80c011cc0ecf751e14a9afcf65e56
elahigill <elahigill@localhost>
2023-10-20 15:46:58 -0600

    All modules

---
+++ src/test/java/steps/Graph.java
@@ -0,0 +1,90 @@
+package steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Graph{
+	String URL = "https://dsportalapp.herokuapp.com/";
+	By Graph_getstarted = By.xpath("//a[@href='graph']");
+	By Graph=By.xpath("//a[@class='list-group-item'][normalize-space()='Graph']");
+	By Graph_rep= By.xpath("//a[normalize-space()='Graph Representations']");
+	By codemirror=By.xpath("//pre[@role='presentation']");
+	By runbtn= By.xpath("(//button[normalize-space()='Run'])[1]");
+	
+	By tryhere_Graph= By.linkText("Try here>>>");
+	By Practise_Questions=By.xpath("//a[normalize-space()='Practice Questions']");
+	By getstarted = By.xpath("//button[@class=\"btn\"]");
+	
+	By signin= By.xpath("//a[normalize-space()='Sign in']");
+	By username=By.id("id_username");
+	By password=By.id("id_password");
+	By login=By.xpath("//input[@value='Login']");
+	
+
+	
+
+	
+	private WebDriver driver;
+	
+	@Given("The user is in the Graph page after logged in")
+	public void the_user_is_in_the_graph_page_after_logged_in() {
+	
+		
+	
+		System.setProperty("webdriver.chrome.driver", "/Users/elahigill/Downloads/chromedriver-mac-arm64/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://dsportalapp.herokuapp.com/");
+		driver.findElement(getstarted).click();
+		driver.findElement(signin).click();
+		driver.findElement(username).sendKeys("elahi");
+		driver.findElement(password).sendKeys("sarvgun123");
+		driver.findElement(login).click();
+		driver.findElement(Graph_getstarted).click();
+		
+	}
+	@When("The user clicks Graph button")
+	public void the_user_clicks_graph_button() {
+	    driver.findElement(Graph).click();
+	   
+	}
+
+	@And("The user clicks try here button for Graph")
+	public void the_user_clicks_try_here_button_for_graph() {
+		driver.findElement(tryhere_Graph).click();
+	}
+
+
+	@Then("The user should be redirected to a page having a tryEditor with a Run button to graph")
+	public void the_user_should_be_redirected_to_a_page_having_a_try_editor_with_a_run_button_to_graph() {
+		driver.close();
+	}
+	@When("The user clicks Graph Representations button")
+	public void the_user_clicks_graph_representations_button() {
+		driver.findElement(Graph_rep).click();
+		
+	}
+
+	
+
+	@And("The user clicks try here button for graph representations")
+	public void the_user_clicks_try_here_button_for_graph_representations() {
+		driver.findElement(tryhere_Graph).click();
+	}
+
+
+
+	@Then("The user should be redirected to a page having a tryEditor with a Run button to graph rep")
+	public void the_user_should_be_redirected_to_a_page_having_a_try_editor_with_a_run_button_to_graph_rep() {
+	   driver.close();
+	}
+
+
+
+	    
+	}
---
+++ src/test/java/steps/queue.java
@@ -0,0 +1,110 @@
+package steps;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class queue {
+	private WebDriver driver;
+	String URL = "https://dsportalapp.herokuapp.com/";
+	
+	By getstartBtn = By.xpath("//button[@class=\"btn\"]");
+	By signinBtn = By.linkText("Sign in");
+	By usrName = By.xpath("//div[@class=\"container\"]/div/div[2]/form/input[2]");
+	By pwd = By.xpath("//div[@class=\"container\"]/div/div[2]/form/input[3]");
+	By login = By.xpath("//div[@class=\"container\"]/div/div[2]/form/input[4]");
+	By qGetstrBtn = By.xpath("//div[@class=\"row row-cols-1 row-cols-md-3 g-4\"]/div[5]/div/div/a");
+	By ImplofQLnk = By.linkText("Implementation of Queue in Python");
+	By tryHere = By.linkText("Try here>>>");
+	By impusingCollecs = By.linkText("Implementation using collections.deque");
+	By pracQstnsLnk = By.linkText("Practice Questions");
+	@Given("The user is in the Queue page after logged in")
+	public void the_user_is_in_the_queue_page_after_logged_in() {
+		 driver = new ChromeDriver();
+		 System.setProperty("webdriver.chrome.driver", "/Users/elahigill/Downloads/chromedriver-mac-arm64/chromedriver");
+	       	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.manage().window().maximize();
+			driver.get(URL);
+			driver.findElement(getstartBtn).click();
+			driver.findElement(signinBtn).click();
+			driver.findElement(usrName).sendKeys("elahi");
+			driver.findElement(pwd).sendKeys("sarvgun123");
+		    driver.findElement(login).click();
+		    driver.findElement(qGetstrBtn).click();
+	}
+
+	@When("clicks on Implementation of Queue in Python user should be redirected to Implementation of Queue in Python page")
+	public void clicks_on_implementation_of_queue_in_python_user_should_be_redirected_to_implementation_of_queue_in_python_page() {
+		driver.findElement(ImplofQLnk).click();
+	}
+
+	@When("The user clicks Try Here button on Implementation of Queue in Python page")
+	public void the_user_clicks_try_here_button_on_implementation_of_queue_in_python_page() {
+		driver.findElement(tryHere).click();
+	}
+
+	@When("clicks on Implementation using collections user should be redirected to Implementation using collections page")
+	public void clicks_on_implementation_using_collections_user_should_be_redirected_to_implementation_using_collections_page() {
+		driver.findElement(impusingCollecs).click();
+	}
+
+	@When("The user clicks Try Here button on Implementation using collections page")
+	public void the_user_clicks_try_here_button_on_implementation_using_collections_page() {
+		driver.findElement(By.linkText("Try here>>>")).click();
+	}
+
+	@When("clicks on Implementation using array user should be redirected to Implementation using array page")
+	public void clicks_on_implementation_using_array_user_should_be_redirected_to_implementation_using_array_page() {
+		 driver.findElement(By.linkText("Implementation using array")).click();
+	}
+
+	@When("The user clicks Try Here button on Implementation using array page")
+	public void the_user_clicks_try_here_button_on_implementation_using_array_page() {
+		driver.findElement(By.linkText("Try here>>>")).click();
+	}
+
+	@Then("The user should be redirected to a page having a tryEditor with a Run button to test1")
+	public void the_user_should_be_redirected_to_a_page_having_a_tryeditor_with_a_run_button_to_test1() {
+		Actions action = new Actions(driver);
+	    WebElement txt1 = driver.findElement(By.xpath("//div[@class=\"CodeMirror-scroll\"]/div[1]/div/div/div/div[4]"));
+	    action.click(txt1).sendKeys("print('Hello')").perform(); 
+	    driver.findElement(By.xpath("//div[@class=\"container\"]/div/form/button")).click();
+	    driver.close();
+	}
+	    @When("clicks on Queue Operations user should be redirected to Queue Operations page")
+	public void clicks_on_queue_operations_user_should_be_redirected_to_queue_operations_page() {
+		driver.findElement(By.linkText("Queue Operations")).click();
+	}
+
+	@When("The user clicks Try Here button on Queue Operations page")
+	public void the_user_clicks_try_here_button_on_queue_operations_page() {
+		driver.findElement(By.linkText("Try here>>>")).click();
+	}
+	@Then("The user should be redirected to a page having a tryEditor with a Run button to test2")
+	public void the_user_should_be_redirected_to_a_page_having_a_tryeditor_with_a_run_button_to_test2() {
+		Actions action = new Actions(driver);
+	    WebElement txt1 = driver.findElement(By.xpath("//div[@class=\"CodeMirror-scroll\"]/div[1]/div/div/div/div[4]"));
+	    action.click(txt1).sendKeys("print('Hello')").perform(); 
+	    driver.findElement(By.xpath("//div[@class=\"container\"]/div/form/button")).click();
+	    
+	}	
+	@Given("The user navigates back to Queue Operations page and clicks on Practice Questions")
+	public void the_user_navigates_back_to_queue_operations_page_and_clicks_on_practice_questions() {
+	    driver.navigate().back();
+	    driver.findElement(pracQstnsLnk).click();
+	}
+
+	@Then("The user is redirected to practice page")
+	public void the_user_is_redirected_to_practice_page() {
+	    System.out.println("The User is in a blank Practice page");
+	    driver.close();
+	    
+	}
+}
