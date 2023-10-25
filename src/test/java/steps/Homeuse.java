b8649d1f694968f2ab83f87ef903c4a258c1a02a
Rajpreet12 <147854707+Rajpreet12@users.noreply.github.com>
2023-10-25 15:04:20 -0600

    Update Homeuse.java

--- src/test/java/steps/Homeuse.java
+++ src/test/java/steps/Homeuse.java
@@ -1,88 +1,62 @@
-package steps;
+	package steps;
 
-import java.time.Duration;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 
-public class Homeuse {
-	private WebDriver driver;
-	
-	String url="https://dsportalapp.herokuapp.com/";
-	
-	By button=By.xpath("//button[@class='btn']");
-    By button1=By.xpath("//button[@class='btn']");
-    By dropdown=By.xpath("//a[@class='nav-link dropdown-toggle']");
-    By signin=By.xpath("//a[normalize-space()='Arrays']");
-    By alert=By.xpath("//div[@role='alert']");
-    
-    
-    
-	@Given("The user opens DS-Algo portal link")
-	public void the_user_opens_ds_algo_portal_link() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sange\\eclipse-workspace_san\\Ds-Algo-Ninja-patels-\\src\\test\\resources\\driver\\chromedriver.exe");
-	        driver = new ChromeDriver();
-	       
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-			driver.manage().window().maximize();
-			driver.get(url);
-			
-			
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
 
-			
+
+
+	public class Homeuse {
+		 private WebDriver driver;
+		 
+		 By clickgetstart = By.xpath("//button[@class='btn']");
+		 By data_alert=By.xpath("//div[@role='alert']");
+	     By datastrudropdown= By.xpath("//a[@class='nav-link dropdown-toggle']");
+	     By click_Array = By.xpath("//a[@href='array']");
+	     By click_Alert = By.xpath("//div[@role='alert']");
+	     By click_Queue = By.xpath("//a[href='queue']");
+	     By click_Register = By.xpath("//a[@href='/register']");
+	     By click_signin=By.xpath("//a[normalize-space()='Sign in']");
+	     By data_getstarted=By.xpath("//a[@href='data-structures-introduction']");
+	     
+	     public  Homeuse(WebDriver driver) {
+	    	
+			this.driver = driver;
+	     }
+	     
+	     public void click_getstart() {
+	    	 driver.findElement(clickgetstart).click();
+	     }
+	     public void data_getstart() {
+	    	 driver.findElement(data_getstarted).click();
+	     }
+	     public void dataalert() {
+	    	 driver.findElement(data_alert).click();
+	     }
+	     
+	     public void datastru_dropdown() {
+	    	 driver.findElement(datastrudropdown).click();
+	     }
+	     public void array() {
+	    	 driver.findElement(click_Array).click();
+	     }
+	     public void alert() {
+	    	 driver.findElement(click_Alert).click();
+	     }
+	     public void queue() {
+	    	 driver.findElement(click_Queue).click();
+	    	 
+	     }
+	     public void click_signin() {
+	    	 driver.findElement(click_signin).click();
+	     }
+	     public void register() {
+	    	 driver.findElement(click_Register).click();
+	     }
 	}
 
-	@And("The user clicks the Get Started button")
-	public void the_user_clicks_the_get_started_button() {
-		driver.findElement(button).click();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	   
-	}
 
-	@And("The user should be redirected to homepage")
-	public void the_user_should_be_redirected_to_homepage() {
-		driver.navigate().back();
-		//driver.close();
-	}
-
-	@When("The user clicks get started button")
-	public void the_user_clicks_get_started_button() {
-		driver.findElement(button1).click();
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	}
-
-	@And("The user clicks Data structure drop down")
-	public void the_user_clicks_data_structure_drop_down() {
-		driver.findElement(dropdown).click();
-	}
-
-	@And("The user selects any data structures item from dropdown without sign in")
-	public void the_user_selects_any_data_structures_item_from_dropdown_without_sign_in() {
-		driver.findElement(signin).click();
-	}
-
-	/*@And("The user clicks signin")
-				public void the_user_clicks_signin() {
-			   	driver.findElement(By.linkText("Sign in")).click();
-	}		   
-		/@Then("The user clicks Register")
-		public void the_user_clicks_Register() {
-			driver.findElement(By.linkText( "Register ")).click();
-	
-	}*/
-	@Then("It should alert the user with a message You are not Logged in.")
-	public void it_should_alert_the_user_with_a_message_you_are_not_logged_in() {
-	    driver.findElement(alert);
-	    driver.close();
-	    
-}
-}
 
 	
